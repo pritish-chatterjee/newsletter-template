759468a8e2638dff90703ebc92e3feb41997b399
pritish-chatterjee <pritishchatterjee96@gmail.com>
2022-02-21 17:56:36 +0530

    minor changes

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.post("/failure", function (req, res) {
   res.redirect("/");
 });
 
-app.listen("3000", function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("App is listening on port 3000");
 });
